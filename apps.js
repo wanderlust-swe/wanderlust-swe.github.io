f0c3a97b1ce2dbe9ee50aa11b3e2b273dfee9c58
wanderlust-swe <comtam0202@gmail.com>
2022-08-26 07:24:47 -0400

    third

--- apps.js
+++ apps.js
@@ -20,6 +20,73 @@ document.getElementById('second').innerHTML = ":" + second;
 }
 setInterval (clock, 1000);
 
+let ml4 = {};
+ml4.opacityIn = [0,1];
+ml4.scaleIn = [0.2, 1];
+ml4.scaleOut = 3;
+ml4.durationIn = 800;
+ml4.durationOut = 600;
+ml4.delay = 500;
 
-let whatDay = new Date().toLocaleDateString('en-US', { weekday:'long' });
-document.getElementById('day').innerHTML = whatDay;
+anime.timeline({loop: true})
+  .add({
+    targets: '.ml4 .letters-1',
+    opacity: ml4.opacityIn,
+    scale: ml4.scaleIn,
+    duration: ml4.durationIn
+  }).add({
+    targets: '.ml4 .letters-1',
+    opacity: 0,
+    scale: ml4.scaleOut,
+    duration: ml4.durationOut,
+    easing: "easeInExpo",
+    delay: ml4.delay
+  }).add({
+    targets: '.ml4 .letters-2',
+    opacity: ml4.opacityIn,
+    scale: ml4.scaleIn,
+    duration: ml4.durationIn
+  }).add({
+    targets: '.ml4 .letters-2',
+    opacity: 0,
+    scale: ml4.scaleOut,
+    duration: ml4.durationOut,
+    easing: "easeInExpo",
+    delay: ml4.delay
+  }).add({
+    targets: '.ml4 .letters-3',
+    opacity: ml4.opacityIn,
+    scale: ml4.scaleIn,
+    duration: ml4.durationIn
+  }).add({
+    targets: '.ml4 .letters-3',
+    opacity: 0,
+    scale: ml4.scaleOut,
+    duration: ml4.durationOut,
+    easing: "easeInExpo",
+    delay: ml4.delay
+  }).add({
+    targets: '.ml4',
+    opacity: 0,
+    duration: 500,
+    delay: 500
+  });
+
+document.getElementById('letters letters-1').innerHTML = m14;
+document.getElementById('letters letters-2').innerHTML = m14;
+document.getElementById('letters letters-3').innerHTML = m14;
+
+
+    // let whatDay = new Date().toLocaleDateString('en-US', { weekday:'long' });
+    
+    function greet () {
+    let date = new Date();
+    let weekDay = getWeekDay(date);
+    document.getElementById('day').innerHTML = weekDay;
+    }
+
+    setInterval (greet, 500);
+
+
+
+    
