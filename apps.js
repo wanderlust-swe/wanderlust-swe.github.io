7d0b4f93ff21270c6caf2b2f05bf21fc12734652
wanderlust-swe <comtam0202@gmail.com>
2022-08-26 22:42:12 -0400

    new features

--- apps.js
+++ apps.js
@@ -20,72 +20,18 @@ document.getElementById('second').innerHTML = ":" + second;
 }
 setInterval (clock, 1000);
 
-let ml4 = {};
-ml4.opacityIn = [0,1];
-ml4.scaleIn = [0.2, 1];
-ml4.scaleOut = 3;
-ml4.durationIn = 800;
-ml4.durationOut = 600;
-ml4.delay = 500;
+    let btn = document.getElementById('btn');
+    let clickbox = document.getElementById('clickbox');
+    let quote = ['Cuddle', 'Watch a Movie Together',
+    'Play Card Games', 'Talk for Hours', 
+    'Play Who Wants To Be a Millionaire',
+    'Watch Big Bang', 'Watch TVB Dramas',
+    'Nap', 'Teach Bei How to Cut Bittermelon Properly' ];
 
-anime.timeline({loop: true})
-  .add({
-    targets: '.ml4 .letters-1',
-    opacity: ml4.opacityIn,
-    scale: ml4.scaleIn,
-    duration: ml4.durationIn
-  }).add({
-    targets: '.ml4 .letters-1',
-    opacity: 0,
-    scale: ml4.scaleOut,
-    duration: ml4.durationOut,
-    easing: "easeInExpo",
-    delay: ml4.delay
-  }).add({
-    targets: '.ml4 .letters-2',
-    opacity: ml4.opacityIn,
-    scale: ml4.scaleIn,
-    duration: ml4.durationIn
-  }).add({
-    targets: '.ml4 .letters-2',
-    opacity: 0,
-    scale: ml4.scaleOut,
-    duration: ml4.durationOut,
-    easing: "easeInExpo",
-    delay: ml4.delay
-  }).add({
-    targets: '.ml4 .letters-3',
-    opacity: ml4.opacityIn,
-    scale: ml4.scaleIn,
-    duration: ml4.durationIn
-  }).add({
-    targets: '.ml4 .letters-3',
-    opacity: 0,
-    scale: ml4.scaleOut,
-    duration: ml4.durationOut,
-    easing: "easeInExpo",
-    delay: ml4.delay
-  }).add({
-    targets: '.ml4',
-    opacity: 0,
-    duration: 500,
-    delay: 500
-  });
-
-document.getElementById('letters letters-1').innerHTML = m14;
-document.getElementById('letters letters-2').innerHTML = m14;
-document.getElementById('letters letters-3').innerHTML = m14;
-
-
-    // let whatDay = new Date().toLocaleDateString('en-US', { weekday:'long' });
-    
-    function greet () {
-    let date = new Date();
-    let weekDay = getWeekDay(date);
-    document.getElementById('day').innerHTML = weekDay;
-    }
-
-    setInterval (greet, 500);
+    btn.addEventListener('click', function(){
+        let randomQuote = quote[Math.floor(Math.random() * quote.length)]
+        clickbox.innerHTML = randomQuote;
+    })
 
 
 
